6aa5b0ca27d62f75e9386765deb40f55dd502520
miya264 <tmiyajima117@gmail.com>
2025-02-11 12:58:41 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: "standalone",
     env: {
         // Reference a variable that was defined in the .env file and make it available at Build Time
         API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
